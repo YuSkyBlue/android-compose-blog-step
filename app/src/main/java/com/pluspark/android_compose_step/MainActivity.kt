094bff7d493a410f8143ed8ad7ba8220e880520b
ckyu <dbcjdrb1997@gmail.com>
2024-03-14 11:16:27 +0900

    [CompositionLocalProvider] initial commit

--- app/src/main/java/com/pluspark/android_compose_step/MainActivity.kt
+++ app/src/main/java/com/pluspark/android_compose_step/MainActivity.kt
@@ -3,15 +3,23 @@ package com.pluspark.android_compose_step
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.pluspark.android_compose_step.ui.theme.AndroidcomposestepTheme
 
+val ColorCompositionLocal = staticCompositionLocalOf {
+    Color.Blue // 기본값을 정의
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                        Composable1()
+//                        Composable2()
+//                        Composable3()
+//                        Composable4()
+//                        Composable5()
+//                        Composable6()
+
+
                 }
             }
         }
@@ -30,17 +46,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Composable1() {
+    Column(Modifier.fillMaxSize()){
+        Text(
+            modifier = Modifier.background(color = ColorCompositionLocal.current),
+            text = "Composable1"
+        )
+        CompositionLocalProvider(ColorCompositionLocal.provides(Color.Cyan)) {
+            Composable2()
+        }
+        CompositionLocalProvider(ColorCompositionLocal.provides(Color.Red)) {
+            Composable4()
+        }
+    }
+
+}
+
+@Composable
+fun Composable2() {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.background(color = ColorCompositionLocal.current),
+        text = "Composable2"
+    )
+    Composable3()
+}
+
+@Composable
+fun Composable3() {
+    Text(
+        modifier = Modifier.background(color = ColorCompositionLocal.current),
+        text = "Composable3"
+    )
+
+}
+
+@Composable
+fun Composable4() {
+    Text(
+        modifier = Modifier.background(color = ColorCompositionLocal.current),
+        text = "Composable4"
+    )
+
+    CompositionLocalProvider(ColorCompositionLocal.provides(Color.Green)) {
+        Composable5()
+    }
+    Composable6()
+}
+@Composable
+fun Composable5() {
+    Text(
+        modifier = Modifier.background(color = ColorCompositionLocal.current),
+        text = "Composable5"
+    )
+}
+@Composable
+fun Composable6() {
+    Text(
+        modifier = Modifier.background(color = ColorCompositionLocal.current),
+        text = "Composable6"
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidcomposestepTheme {
-        Greeting("Android")
-    }
-}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    AndroidcomposestepTheme {
+//    }
+//}
